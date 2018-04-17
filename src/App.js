48892b01be66fb9e1385bf61567d9e3bdc0dd1f1
Andreas Teneketzoglou <antekai00@gmail.com>
2018-04-17 14:21:40 +0200

    create nav

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Recipes from "./Recipes";
+import AddRecipe from "./AddRecipe";
+import Nav from "./Nav";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Router>
+          <div>
+            <Nav />
+            <Switch>
+              <Route exact path="/" component={Recipes} />
+              <Route path="/addrecipe" component={AddRecipe} />
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
---
+++ src/Nav.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Link from "react-router-dom";
+import { css } from "glamor";
+
+export default class Nav extends React.Component {
+  render() {
+    return (
+      <div {...css(styles.container)}>
+        <h1 {...css(styles.heading)}>RecipeApp</h1>
+        <Link to="/" {...css(styles.link)}>
+          Recipes
+        </Link>
+        <Link to="/addrecipe" {...css(styles.link)}>
+          Add Recipe
+        </Link>
+      </div>
+    );
+  }
+}
+
+const styles = {
+  link: {
+    textDecoration: "none",
+    marginLeft: 15,
+    color: "white",
+    ":hover": {
+      textDecoration: "underline"
+    }
+  },
+  container: {
+    display: "flex",
+    backgroundColor: "#00c334",
+    padding: "0px 30px",
+    alignItems: "center"
+  },
+  heading: {
+    color: "white",
+    paddingRight: 20
+  }
+};
