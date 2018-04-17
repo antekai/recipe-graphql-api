c130cc4c669ea562a07e8f3eaac691e13a1e90d0
Andreas Teneketzoglou <antekai00@gmail.com>
2018-04-17 21:21:09 +0200

    add query ListRecipes

---
+++ src/queries/ListRecipes.js
@@ -0,0 +1,14 @@
+import gql from "graphql-tag";
+
+export default gql`
+  query listRecipes {
+    listRecipes {
+      items {
+        name
+        id
+        ingredients
+        instructions
+      }
+    }
+  }
+`;
