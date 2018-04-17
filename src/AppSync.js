3a3557145ae077103de3ff871736dde5c53e36e6
Andreas Teneketzoglou <antekai00@gmail.com>
2018-04-17 14:05:39 +0200

    connect appsync

---
+++ src/AppSync.js
@@ -0,0 +1,6 @@
+﻿export default {
+    "graphqlEndpoint": "https://sunyxbubkff3joawx4nj6e3qnu.appsync-api.us-east-1.amazonaws.com/graphql",
+    "region": "us-east-1",
+    "authenticationType": "API_KEY",
+    "apiKey": "da2-abgkwzvfxfdp7hgszskexmc64a"
+}
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+import AWSAppSyncClient from "aws-appsync";
+import { Rehydrated } from "aws-appsync-react";
+import { ApolloProvider } from "react-apollo";
+
+import appSyncConfig from "./appsync";
+
+const client = new AWSAppSyncClient({
+  url: appSyncConfig.graphqlEndpoint,
+  region: appSyncConfig.region,
+  auth: {
+    type: appSyncConfig.authenticationType,
+    apiKey: appSyncConfig.apiKey
+  }
+});
+
+const WithProvider = () => (
+  <ApolloProvider client={client}>
+    <Rehydrated
+      render={({ rehydrated }) =>
+        rehydrated ? <App /> : <strong>Loading...</strong>
+      }
+    />
+  </ApolloProvider>
+);
+
+ReactDOM.render(<WithProvider />, document.getElementById("root"));
