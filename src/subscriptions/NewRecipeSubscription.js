5bda332e7067e8a69c0d2e47f38b065645da5b65
Andreas Teneketzoglou <antekai00@gmail.com>
2018-04-17 21:49:14 +0200

    add new Recipe subscription

---
+++ src/subscriptions/NewRecipeSubscription.js
@@ -0,0 +1,12 @@
+import gql from "graphql-tag";
+
+export default gql`
+  subscription NewRecipeSub {
+    onCreateRecipe {
+      name
+      id
+      ingredients
+      instructions
+    }
+  }
+`;
